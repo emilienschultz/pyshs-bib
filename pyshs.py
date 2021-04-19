0e622bd4a5f31a0ddf7703cbf73696c7ee28da3e
Emilien Schultz <emilien@iMac-de-Emilien.local>
2021-04-19 20:32:12 +0200

    modif version

--- pyshs.py
+++ pyshs.py
@@ -15,7 +15,7 @@ import pandas as pd
 import numpy as np
 from scipy.stats import chi2_contingency
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 def tri_a_plat(df,variable,weight = False):
     """
