fb7c6d5d19e4983dfb22fdb9d11c68622ceb9c9a
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-07-10 17:21:37 +0200

    Move posh into it's own directory

---
+++ pyshs/__init__.py
@@ -0,0 +1,49 @@
+"""
+PySHS - Faciliter le traitement de données de questionnaires en SHS
+Langue : Français
+Dernière modification : 09/07/2022
+Auteur : Émilien Schultz
+Contributeurs.rices :
+- Matthias Bussonnier
+- Léo Mignot
+- Fatima Gauna
+
+Pour le moment le module PySHS comprend :
+
+- une fonction de description du tableau
+- une fonction de comparaison de deux colonnes pour voir le recodage
+- une fonction pour le tri à plat (pondéré)
+- une fonction pour les tableaux croisés (pondérés)
+- une fonction pour des tableaux croisés multiples (pondérés) afin de voir le lien variable dépendante/indépendantes
+- une fonction pour un tableau croisé à trois variables pour en contrôler une lors de l'analyse
+- une fonction de mise en forme des résultats de la régression logistique de Statsmodel pour avoir un tableau avec les références
+- une fonction pour produire la régression logistique binomiale
+- une fonction moyenne & écart-type pondéré
+- une fonction d'écriture de tableaux excels
+
+Temporairement :
+- une fonction de mise en forme différente de la régression logistique, incluant les effets d'interaction (beta)
+- une fonction de test du ratio de vraissemblance de deux régressions
+
+"""
+
+from ._core import (
+    description,
+    tri_a_plat,
+    verification_recodage,
+    tableau_croise,
+    tableau_croise_controle,
+    tableau_croise_multiple,
+    significativite,
+    tableau_reg_logistique,
+    construction_formule,
+    regression_logistique,
+    likelihood_ratio,
+    vers_excel,
+    moyenne_ponderee,
+    ecart_type_pondere,
+    tableau_reg_logistique_distribution,
+    cramers_corrected_stat,
+)
+
+__version__ = "0.2.4"
--- pyshs/_core.py
+++ pyshs/_core.py
@@ -1,31 +1,3 @@
-"""
-PySHS - Faciliter le traitement de données de questionnaires en SHS
-Langue : Français
-Dernière modification : 09/07/2022
-Auteur : Émilien Schultz
-Contributeurs.rices :
-- Matthias Bussonnier
-- Léo Mignot
-- Fatima Gauna
-
-Pour le moment le module PySHS comprend :
-
-- une fonction de description du tableau
-- une fonction de comparaison de deux colonnes pour voir le recodage
-- une fonction pour le tri à plat (pondéré)
-- une fonction pour les tableaux croisés (pondérés)
-- une fonction pour des tableaux croisés multiples (pondérés) afin de voir le lien variable dépendante/indépendantes
-- une fonction pour un tableau croisé à trois variables pour en contrôler une lors de l'analyse
-- une fonction de mise en forme des résultats de la régression logistique de Statsmodel pour avoir un tableau avec les références
-- une fonction pour produire la régression logistique binomiale
-- une fonction moyenne & écart-type pondéré
-- une fonction d'écriture de tableaux excels
-
-Temporairement :
-- une fonction de mise en forme différente de la régression logistique, incluant les effets d'interaction (beta)
-- une fonction de test du ratio de vraissemblance de deux régressions
-
-"""
 import warnings
 import math
 
@@ -42,7 +14,6 @@ import statsmodels.formula.api as smf
 import plotly.graph_objects as go
 
 
-__version__ = "0.2.4"
 
 
 def description(df):
